cc2b47b734a8c498374a4433277070b211ae7ec5
chemicalfiend <teamkagaku101@gmail.com>
2023-07-17 21:32:16 +0530

    Added a Bare propagator test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using QuantumDynamics
 using Test
 
-@testset "QuantumDynamics.jl" begin
-    # Write your tests here.
+@testset "Isolated Hamiltonian" begin
+    H = [-0.1im -1.0; -1.0 -0.5im]
+    V(t) = 12*cos(10.0*t)
+    
+    EF = Utilities.ExternalField(V, [1.0+0.0im 0.0; 0.0 -1.0])
+
+    ρ0 = [1.0+1.0im 0.0; 0.0 0.0]
+    dt = 0.125
+    ntimes = 100
+
+    times, ρs = Bare.propagate(; Hamiltonian=H, ρ0, dt, ntimes, external_fields=[EF])
 end
+
+
