53c66d72d7f777a4435df63f358a13e4ccf0c7f7
Documenter.jl <documenter@juliadocs.github.io>
2024-02-19 08:42:58 +0000

    build based on f797af9

--- dev/tutorial_examples/flux_rate_theory.jl
+++ dev/tutorial_examples/flux_rate_theory.jl
@@ -1,26 +1,6 @@
 using QuantumDynamics
-using LaTeXStrings
-import PyPlot;
-const plt = PyPlot;
 
 invcm2au = 4.55633e-6
-
-function new_figure(plot_type="full")
-    if plot_type == "full"
-        fig = plt.figure(; figsize=(3.175, 2.25))
-    elseif plot_type == "half"
-        fig = plt.figure(; figsize=(1.56, 1.4))
-    end
-    ax = fig.add_subplot(111)
-
-    for axis in ["top", "bottom", "left", "right"]
-        ax.spines[axis].set_linewidth(0.1)
-        ax.spines[axis].set_color("gray")
-    end
-    ax.tick_params(width=0.1, direction="in", color="gray")
-
-    fig, ax
-end
 
 H = Matrix{ComplexF64}([
     0.0 0.000525
@@ -37,26 +17,26 @@ idmat = [1.0 0.0; 0.0 1.0]
 
 Jw = [SpectralDensities.ExponentialCutoff(; ξ=0.1, ωc=500invcm2au)]
 At, avgbond = ComplexTimePI.A_of_t(; Hamiltonian=H, β, t=0.0, N=50, Jw, svec, A=idmat)
-Q = real(tr(At*state1))
+Q = real(tr(At * state1))
 time_xi01, corr_xi01, avg_bond_dim_xi01 = ComplexTimePI.correlation_function_tnpi(; Hamiltonian=H, β, tfinal=8000.0, dt=100.0, N=50, Jw, svec, A, B=[B], Z=Q, verbose=true)
 
 Jw = [SpectralDensities.ExponentialCutoff(; ξ=0.5, ωc=500invcm2au)]
 At, avgbond = ComplexTimePI.A_of_t(; Hamiltonian=H, β, t=0.0, N=50, Jw, svec, A=idmat)
-Q = real(tr(At*state1))
+Q = real(tr(At * state1))
 time_xi05, corr_xi05, avg_bond_dim_xi05 = ComplexTimePI.correlation_function_tnpi(; Hamiltonian=H, β, tfinal=8000.0, dt=100.0, N=50, Jw, svec, A, B=[B], Z=Q, verbose=true)
 
-new_figure()
-plt.plot(0.002278165 * time_xi01, real.(corr_xi01) ./ H[1,2], lw=0.75, label=L"\xi=0.1")
-plt.plot(0.002278165 * time_xi05, real.(corr_xi05) ./ H[1,2], lw=0.75, label=L"\xi=0.5")
+new_figure("double")
+plt.plot(0.002278165 * time_xi01, real.(corr_xi01) ./ H[1, 2], lw=0.75, label=L"\xi=0.1")
+plt.plot(0.002278165 * time_xi05, real.(corr_xi05) ./ H[1, 2], lw=0.75, label=L"\xi=0.5")
 plt.legend()
 plt.xlabel(L"\omega_c t")
 plt.ylabel(L"C_{fs}(t) / \Omega")
-plt.savefig("rate_theory.pdf", bbox_inches="tight")
+plt.savefig("rate_theory.png", bbox_inches="tight")
 
-new_figure()
+new_figure("double")
 plt.plot(0.002278165 * time_xi01, avg_bond_dim_xi01, lw=0.75, label=L"\xi=0.1")
 plt.plot(0.002278165 * time_xi05, avg_bond_dim_xi05, lw=0.75, label=L"\xi=0.5")
 plt.legend()
 plt.xlabel(L"\omega_c t")
 plt.ylabel(L"\bar{\beta}(t)")
-plt.savefig("bond_dim_rate_theory.pdf", bbox_inches="tight")
+plt.savefig("bond_dim_rate_theory.png", bbox_inches="tight")
