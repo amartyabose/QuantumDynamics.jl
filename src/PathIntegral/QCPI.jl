97d1fffb0774e82a9c93e6b95c6d07b96118e7e6
Amartya Bose <amartya.bose@gmail.com>
2024-05-16 09:48:58 +0530

    Debugging 2

--- src/PathIntegral/QCPI.jl
+++ src/PathIntegral/QCPI.jl
@@ -11,43 +11,43 @@ function propagate(; Hamiltonian::Matrix{ComplexF64}, Jw::AbstractVector{<:Spect
     sdim = size(Hamiltonian, 1)
     ρs = zeros(ComplexF64, ntimes + 1, sdim, sdim)
     time = 0:dt:ntimes*dt
-    # @assert length(Jw) == size(svec, 1)
+    @assert length(Jw) == size(svec, 1)
     if kmax > ntimes
         kmax = ntimes + 2
     end
-    if reference_choice=="fixed"
-        η = [EtaCoefficients.calculate_η(jw; β=solvent.β, dt, kmax, imaginary_only=true) for jw in Jw]
-        for (i, ps) in enumerate(solvent)
-            if verbose
-                @info "Initial condition number $i started."
-            end
-            fbU, srefs = Propagators.calculate_reference_propagators(; Hamiltonian, solvent, ps, ρ0, classical_dt, dt, ntimes, reference_choice)
-            if verbose
-                @info "Propagators calculated."
-            end
-            _, ρ = path_integral_routine(; fbU, η, β=solvent.β, ρ0, dt, ntimes, kmax, extraargs, svec, verbose)
-            ρs .+= ρ
-            if verbose
-                @info "Initial condition number $i done."
-            end
+    # if reference_choice=="fixed"
+    #     η = [EtaCoefficients.calculate_η(jw; β=solvent.β, dt, kmax, imaginary_only=true) for jw in Jw]
+    #     for (i, ps) in enumerate(solvent)
+    #         if verbose
+    #             @info "Initial condition number $i started."
+    #         end
+    #         fbU, srefs = Propagators.calculate_reference_propagators(; Hamiltonian, solvent, ps, ρ0, classical_dt, dt, ntimes, reference_choice)
+    #         if verbose
+    #             @info "Propagators calculated."
+    #         end
+    #         _, ρ = path_integral_routine(; fbU, η, β=solvent.β, ρ0, dt, ntimes, kmax, extraargs, svec, verbose)
+    #         ρs .+= ρ
+    #         if verbose
+    #             @info "Initial condition number $i done."
+    #         end
+    #     end
+    # else
+    ζ = [EtaCoefficients.calculate_ζ(jw; dt, kmax) for jw in Jw]
+    for (i, ps) in enumerate(solvent)
+        if verbose
+            @info "Initial condition number $i started."
         end
-    else
-        ζ = [EtaCoefficients.calculate_ζ(jw; dt, kmax) for jw in Jw]
-        for (i, ps) in enumerate(solvent)
-            if verbose
-                @info "Initial condition number $i started."
-            end
-            fbU, srefs = Propagators.calculate_reference_propagators(; Hamiltonian, solvent, ps, ρ0, classical_dt, dt, ntimes, reference_choice)
-            if verbose
-                @info "Propagators calculated."
-            end
-            _, ρ = path_integral_routine(; fbU, ζ, ρ0, dt, ntimes, kmax, extraargs, svec, srefs, verbose)
-            ρs .+= ρ
-            if verbose
-                @info "Initial condition number $i done."
-            end
+        fbU, srefs = Propagators.calculate_reference_propagators(; Hamiltonian, solvent, ps, ρ0, classical_dt, dt, ntimes, reference_choice)
+        if verbose
+            @info "Propagators calculated."
+        end
+        _, ρ = path_integral_routine(; fbU, ζ, ρ0, dt, ntimes, kmax, extraargs, svec, srefs, verbose)
+        ρs .+= ρ
+        if verbose
+            @info "Initial condition number $i done."
         end
     end
+    # end
     time, ρs ./ solvent.num_samples
 end
 
