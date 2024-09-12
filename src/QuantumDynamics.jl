979f747f4f78d7af49122d7c7a765cce70722615
Amartya Bose <amartya.bose@gmail.com>
2024-09-13 01:41:10 +0530

    correcting change of sub-modules

--- src/QuantumDynamics.jl
+++ src/QuantumDynamics.jl
@@ -13,10 +13,14 @@ export Utilities
 
 include("Environment/Environment.jl")
 include("DynamicMap_MasterEquation/TTM.jl")
+export TTM
 include("HEOM/HEOM.jl")
 include("PathIntegral/pathintegral.jl")
 include("Approximate/Approximate.jl")
 include("DynamicMap_MasterEquation/GQME.jl")
+export GQME
+include("DynamicMap_MasterEquation/Spectroscopy.jl")
+export Spectroscopy
 
 include("AbInitio/AbInitio.jl")
 export AbInitio
