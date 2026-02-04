7458f806244aa5c4d92705ecc7e13a4f1bf950b7
Visuwesh <visuweshm@gmail.com>
2026-02-04 12:47:34 +0530

    Add the generic implementation for bath propagation

--- src/Environment/SolventsX.jl
+++ src/Environment/SolventsX.jl
@@ -51,11 +51,14 @@ end
 Base.eltype(::HarmonicBathX) = HarmonicPhaseSpaceX
 Base.length(b::HarmonicBathX) = b.nsamples
 
-"""
+raw"""
     propagate_forced_bath(bath::HarmonicBathX, bps::HarmonicPhaseSpaceX,
                           f::Vector{Vector{Float64}}, dt::Real, _::Integer)
 
 Propagate the `bath` subject to constant force `f` from the system.
+
+This assumes the Hamiltonian of the oscillator is of the form
+``H = \frac{p^2}{2} + \frac{\omega^2 q^2}{2} - f q.``
 """
 function propagate_forced_bath(bath::HarmonicBathX, bps::HarmonicPhaseSpaceX,
                                f::Vector{Vector{Float64}}, dt::Real, _::Integer)
@@ -71,6 +74,49 @@ function propagate_forced_bath(bath::HarmonicBathX, bps::HarmonicPhaseSpaceX,
     end
 
     HarmonicPhaseSpaceX(q, p)
+end
+
+"""
+    bath_force(bath::Solvent, state::PhaseSpace, n::Integer)
+
+Return the `n`th bath's (self-)force at the phase space point given by `state`.
+"""
+function bath_force(bath::Solvent, state::PhaseSpace, n::Integer) end
+
+bath_force(bath::HarmonicBathX, state::HarmonicPhaseSpaceX, n::Integer) = -bath.ω[n]^2 * state.q[n]
+
+raw"""
+    propagate_forced_bath(bath::Solvent, state::PhaseSpace,
+                          f::Vector{Vector{Float64}}, dt::Real,
+                          ntimes::Integer)
+
+Propagate `bath` subject to constant force `f` from the system.
+
+The Hamiltonian for a single bath is assumed to be of the form
+``H = \sum_i \frac{p_i^2}{2} + V(\{q_i\}) - f_i q_i``
+where `V(\{q_i\})` is the state-independent potential of the bath.
+
+The time step of the propagation is given by `dt`, and the number of
+Verlet steps by `ntimes`.
+"""
+function propagate_forced_bath(bath::Solvent, state::PhaseSpace,
+                               f::Vector{Vector{Float64}}, dt::Real,
+                               ntimes::Integer)
+    q = similar.(state.q)
+    p = similar.(state.p)
+
+    @inbounds for _ in 1:ntimes
+        for b in eachindex(state.q)
+            @. p[b] += 0.5 * (bath_force(bath, state, b) + f[b]) * dt
+            @. q[b] += p[b] * dt
+        end
+        state = eltype(bath)(q, p)
+        for b in eachindex(state.p)
+            @. p[b] += 0.5 * (bath_force(bath, state, b) + f[b]) * dt
+        end
+    end
+
+    eltype(bath)(q, p)
 end
 
 end
